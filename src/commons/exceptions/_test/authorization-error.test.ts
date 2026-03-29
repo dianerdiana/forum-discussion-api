f52dcaa66f0564105b6877902fa502a4e721c005
dianerdiana <dianerdiana.de@gmail.com>
2026-03-29 17:19:42 +0700

    feat: Implement exception handling classes and configuration setup

---
+++ src/commons/exceptions/_test/authorization-error.test.ts
@@ -0,0 +1,16 @@
+import { AuthorizationError } from '../authorization-error.js';
+import { ClientError } from '../client-error.js';
+
+describe('AuthorizationError', () => {
+  it('should create AuthorizationError correctly', () => {
+    const authenticationError = new AuthorizationError('authorization error!');
+
+    expect(authenticationError).toBeInstanceOf(AuthorizationError);
+    expect(authenticationError).toBeInstanceOf(ClientError);
+    expect(authenticationError).toBeInstanceOf(Error);
+
+    expect(authenticationError.statusCode).toEqual(403);
+    expect(authenticationError.message).toEqual('authorization error!');
+    expect(authenticationError.name).toEqual('AuthorizationError');
+  });
+});
