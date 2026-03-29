a929cefa0717a05b87b4b5c01642be39d00db030
dianerdiana <dianerdiana.de@gmail.com>
2026-03-29 17:30:19 +0700

    feat(auth): implement AuthenticationRepository and NewAuth entities with tests

--- src/commons/exceptions/_test/client-error.test.ts
+++ src/commons/exceptions/_test/client-error.test.ts
@@ -2,6 +2,6 @@ import { ClientError } from '../client-error.js';
 
 describe('ClientError', () => {
   it('should throw error when directly use it', () => {
-    expect(() => new ClientError('')).toThrowError('cannot instantiate abstract class');
+    expect(() => new ClientError('')).toThrow('cannot instantiate abstract class');
   });
 });
---
+++ src/domains/authentications/entities/new-auth.ts
@@ -0,0 +1,28 @@
+export type NewAuthPayload = {
+  accessToken?: string;
+  refreshToken?: string;
+};
+
+export class NewAuth {
+  accessToken: string | undefined;
+  refreshToken: string | undefined;
+
+  constructor(payload: NewAuthPayload) {
+    this._verifyPayload(payload);
+
+    this.accessToken = payload.accessToken;
+    this.refreshToken = payload.refreshToken;
+  }
+
+  _verifyPayload(payload: NewAuthPayload) {
+    const { accessToken, refreshToken } = payload;
+
+    if (!accessToken || !refreshToken) {
+      throw new Error('NEW_AUTH.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof accessToken !== 'string' || typeof refreshToken !== 'string') {
+      throw new Error('NEW_AUTH.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
