8fbf16a56bb1f3f1fd54731b9a5b3ce7e6a72300
dianerdiana <dianerdiana.de@gmail.com>
2026-03-29 17:21:01 +0700

    feat (users): add user entity classes with validation

---
+++ src/domains/users/repositories/user.repository.ts
@@ -0,0 +1,17 @@
+export class UserRepository {
+  async addUser(_registerUser: unknown) {
+    throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyAvailableUsername(_username: string) {
+    throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getPasswordByUsername(_username: string) {
+    throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getIdByUsername(_username: string) {
+    throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
