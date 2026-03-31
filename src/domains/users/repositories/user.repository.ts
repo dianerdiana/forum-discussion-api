fd4ecf8c45a097d22a290ed9a013d452642804bd
dianerdiana <dianerdiana.de@gmail.com>
2026-03-31 17:55:02 +0700

    feat(user): add User class and repository interface for user management

---
+++ src/domains/users/repositories/user.repository.ts
@@ -0,0 +1,8 @@
+import type { User } from '../entities/user.js';
+
+export interface UserRepository {
+  addUser: (user: User) => Promise<void>;
+  isUsernameAvailable: (username: string) => Promise<boolean>;
+  getPasswordByUsername: (username: string) => Promise<string>;
+  getIdByUsername: (username: string) => Promise<string>;
+}
