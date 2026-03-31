310c1f9c0cd2cb480ac104b515449597379a9601
dianerdiana <dianerdiana.de@gmail.com>
2026-03-31 20:28:12 +0700

    feat(exceptions): add centralized exception exports for error handling

---
+++ src/commons/exceptions/index.ts
@@ -0,0 +1,6 @@
+export { AuthenticationError } from './authentication-error.js';
+export { AuthorizationError } from './authorization-error.js';
+export { ClientError } from './client-error.js';
+export { DomainErrorTranslator } from './domain-error-translator.js';
+export { InvariantError } from './invariant-error.js';
+export { NotFoundError } from './not-found-error.js';
