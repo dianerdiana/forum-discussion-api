296e74a6a5bd55172aacd259027154f2ae7bbe41
dianerdiana <dianerdiana.de@gmail.com>
2026-03-31 21:43:41 +0700

    refactor(repositories): standardize method signatures in AuthenticationRepository and UserRepository interfaces

--- src/domains/index.ts
+++ src/domains/index.ts
@@ -1,5 +1,3 @@
-// Domain Entities
+export * from './common/index.js';
 export * from './entities/index.js';
-
-// Domain Repositories
 export * from './repositories/index.js';
--- src/domains/repositories/user.repository.ts
+++ src/domains/repositories/user.repository.ts
@@ -1,8 +1,8 @@
 import type { User } from '../entities/user.js';
 
 export interface UserRepository {
-  addUser: (user: User) => Promise<void>;
-  isUsernameAvailable: (username: string) => Promise<boolean>;
-  getPasswordByUsername: (username: string) => Promise<string>;
-  getIdByUsername: (username: string) => Promise<string>;
+  addUser(user: User): Promise<void>;
+  existsUsername(username: string): Promise<boolean>;
+  getPasswordByUsername(username: string): Promise<string>;
+  getIdByUsername(username: string): Promise<string>;
 }
