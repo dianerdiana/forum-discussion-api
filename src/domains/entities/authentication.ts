d35d40e68f4af1f677d0fc0d2e6e7281ca3e34f6
dianerdiana <dianerdiana.de@gmail.com>
2026-04-01 12:51:53 +0700

    refactor(authentication): update Authentication class to use AuthenticationProps interface and improve token handling

--- src/domains/entities/authentication.ts
+++ src/domains/entities/authentication.ts
@@ -1,7 +1,31 @@
+import { Token } from '../value-objects/index.js';
+
+export interface AuthenticationProps {
+  token: Token;
+}
+
 export class Authentication {
-  constructor(public readonly token: string) {
-    if (!token) {
-      throw new Error('AUTHENTICATION_TOKEN_REQUIRED');
-    }
+  constructor(public readonly props: AuthenticationProps) {}
+
+  get token(): Token {
+    return this.props.token;
+  }
+
+  // Factory methods
+  static create(props: { token: string }): Authentication {
+    return new Authentication({ token: Token.create(props.token) });
+  }
+
+  static reconstitute(props: { token: string }): Authentication {
+    return new Authentication({
+      token: Token.create(props.token),
+    });
+  }
+
+  // For persistence
+  toPersistence(): { token: string } {
+    return {
+      token: this.props.token.value,
+    };
   }
 }
