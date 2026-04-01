dc494b99f6fa82fb809cb141be1f917bf6cd08b0
dianerdiana <dianerdiana.de@gmail.com>
2026-04-01 12:52:02 +0700

    refactor(authentication): update import path and modify save method signature in AuthenticationRepository

--- src/domains/repositories/authentication.repository.ts
+++ src/domains/repositories/authentication.repository.ts
@@ -1,7 +1,7 @@
-import type { Authentication } from '../entities/authentication.js';
+import type { Authentication } from '../entities/index.js';
 
 export interface AuthenticationRepository {
-  addToken(auth: Authentication): Promise<void>;
+  save(auth: Authentication): Promise<Authentication>;
   existsToken(token: string): Promise<boolean>;
   deleteToken(token: string): Promise<void>;
 }
