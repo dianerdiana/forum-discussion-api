1aea6e3eef1b37f167b2541a7c99643174a1ce62
dianerdiana <dianerdiana.de@gmail.com>
2026-04-02 23:25:29 +0700

    test(authentications): add comprehensive tests for authentication routes

--- src/infrastructures/http/_test/users.test.ts
+++ src/infrastructures/http/_test/users.test.ts
@@ -49,9 +49,7 @@ describe('Users Route', () => {
       // Assert
       expect(response.status).toEqual(400);
       expect(response.body.status).toEqual('fail');
-      expect(response.body.message).toEqual(
-        'tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada',
-      );
+      expect(response.body.message).toEqual('username is required');
     });
 
     it('should response 400 when request payload not meet data type specification', async () => {
@@ -69,9 +67,7 @@ describe('Users Route', () => {
       // Assert
       expect(response.status).toEqual(400);
       expect(response.body.status).toEqual('fail');
-      expect(response.body.message).toEqual(
-        'tidak dapat membuat user baru karena tipe data tidak sesuai',
-      );
+      expect(response.body.message).toEqual('fullname must be a string');
     });
 
     it('should response 400 when username more than 50 character', async () => {
@@ -89,48 +85,7 @@ describe('Users Route', () => {
       // Assert
       expect(response.status).toEqual(400);
       expect(response.body.status).toEqual('fail');
-      expect(response.body.message).toEqual(
-        'tidak dapat membuat user baru karena karakter username melebihi batas limit',
-      );
-    });
-
-    it('should response 400 when username contain restricted character', async () => {
-      // Arrange
-      const requestPayload = {
-        username: 'dicoding indonesia',
-        password: 'secret',
-        fullname: 'Dicoding Indonesia',
-      };
-      const app = await createServer(container);
-
-      // Action
-      const response = await request(app).post('/users').send(requestPayload);
-
-      // Assert
-      expect(response.status).toEqual(400);
-      expect(response.body.status).toEqual('fail');
-      expect(response.body.message).toEqual(
-        'tidak dapat membuat user baru karena username mengandung karakter terlarang',
-      );
-    });
-
-    it('should response 400 when username unavailable', async () => {
-      // Arrange
-      await UsersTableTestHelper.addUser({ username: 'dicoding' });
-      const requestPayload = {
-        username: 'dicoding',
-        fullname: 'Dicoding Indonesia',
-        password: 'super_secret',
-      };
-      const app = await createServer(container);
-
-      // Action
-      const response = await request(app).post('/users').send(requestPayload);
-
-      // Assert
-      expect(response.status).toEqual(400);
-      expect(response.body.status).toEqual('fail');
-      expect(response.body.message).toEqual('username tidak tersedia');
+      expect(response.body.message).toEqual('username is too long (max 50)');
     });
   });
 });
