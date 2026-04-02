a50615fa9484ea20d98d14a1405c506db32b6530
dianerdiana <dianerdiana.de@gmail.com>
2026-04-02 23:25:38 +0700

    feat(authentication): add validation middleware and schemas for authentication routes

--- src/interfaces/http/api/authentication/routes.ts
+++ src/interfaces/http/api/authentication/routes.ts
@@ -1,13 +1,28 @@
 import express from 'express';
 
+import { ValidatorMiddleware } from '../../middleware/validator.middleware.js';
+
 import type { AuthenticationHandler } from './handler.js';
+import { authenticationSchema, loginSchema } from './schema.js';
 
 export const createAuthenticationRouter = (handler: AuthenticationHandler) => {
   const router = express.Router();
 
-  router.post('/', handler.postAuthenticationHandler);
-  router.put('/', handler.putAuthenticationHandler);
-  router.delete('/', handler.deleteAuthenticationHandler);
+  router.post(
+    '/',
+    ValidatorMiddleware.validateBody(loginSchema),
+    handler.postAuthenticationHandler,
+  );
+  router.put(
+    '/',
+    ValidatorMiddleware.validateBody(authenticationSchema),
+    handler.putAuthenticationHandler,
+  );
+  router.delete(
+    '/',
+    ValidatorMiddleware.validateBody(authenticationSchema),
+    handler.deleteAuthenticationHandler,
+  );
 
   return router;
 };
