560b5d9818eefe67b5a3fc772ac1c5e941f84436
dianerdiana <dianerdiana.de@gmail.com>
2026-04-02 23:25:44 +0700

    feat(user): add user creation schema and validation middleware

--- src/interfaces/http/api/user/routes.ts
+++ src/interfaces/http/api/user/routes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 
+import { ValidatorMiddleware } from '../../middleware/validator.middleware.js';
+
 import type { UserHandler } from './handler.js';
+import { createUserSchema } from './schema.js';
 
 export const createUserRouter = (handler: UserHandler) => {
   const router = express.Router();
 
-  router.post('/', handler.postUserHandler);
+  router.post('/', ValidatorMiddleware.validateBody(createUserSchema), handler.postUserHandler);
 
   return router;
 };
