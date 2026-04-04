8cd19f43c343129a1f3835fc415ae968e345b969
dianerdiana <dianerdiana.de@gmail.com>
2026-04-04 10:35:32 +0700

    refactor(logout-user): improve DTO validation and streamline token handling

--- src/applications/use-cases/authentications/logout-user.use-case.ts
+++ src/applications/use-cases/authentications/logout-user.use-case.ts
@@ -1,5 +1,7 @@
 import type { AuthenticationDto } from '@/applications/dtos/index.js';
 
+import { InvariantError } from '@/commons/index.js';
+
 import type { AuthenticationDomainService, AuthenticationRepository } from '@/domains/index.js';
 
 export class LogoutUserUseCase {
@@ -18,8 +20,17 @@ export class LogoutUserUseCase {
   }
 
   async execute(authDto: AuthenticationDto) {
-    await this.authenticationDomainService.verifyExistingToken(authDto.refreshToken);
+    const { refreshToken } = this.validateDto(authDto);
+    await this.authenticationDomainService.verifyExistingToken(refreshToken);
 
-    await this.authenticationRepository.deleteToken(authDto.refreshToken);
+    await this.authenticationRepository.deleteToken(refreshToken);
+  }
+
+  private validateDto({ refreshToken }: AuthenticationDto): { refreshToken: string } {
+    if (!refreshToken) throw new InvariantError('refresh token is required');
+    if (typeof refreshToken !== 'string')
+      throw new InvariantError('refresh token must be a string');
+
+    return { refreshToken };
   }
 }
