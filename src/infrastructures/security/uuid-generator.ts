97ea548c5b9e906eddcb8a428b14f14a6a17ac32
dianerdiana <dianerdiana.de@gmail.com>
2026-04-04 10:19:14 +0700

    refactor(uuid-generator): remove unused UuidGenerator class and its dependencies

--- src/infrastructures/security/uuid-generator.ts
+++
@@ -1,9 +0,0 @@
-import { v7 as uuidV7 } from 'uuid';
-
-import type { IdGenerator } from '@/domains/common/id-generator.js';
-
-export class UuidGenerator implements IdGenerator {
-  generate(): string {
-    return uuidV7();
-  }
-}
