9c10e58bfa871a0cab0589204a9aa12a6485b946
dianerdiana <dianerdiana.de@gmail.com>
2026-04-04 09:03:11 +0700

    refactor(user): remove validation middleware and schemas from user routes

--- src/interfaces/http/api/user/routes.ts
+++ src/interfaces/http/api/user/routes.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 
-import { ValidatorMiddleware } from '../../middleware/validator.middleware.js';
-
 import type { UserHandler } from './handler.js';
-import { createUserSchema } from './schema.js';
 
 export const createUserRouter = (handler: UserHandler) => {
   const router = express.Router();
 
-  router.post('/', ValidatorMiddleware.validateBody(createUserSchema), handler.postUserHandler);
+  router.post('/', handler.postUserHandler);
 
   return router;
 };
