6ef53f0b9fac694c9d354220a3efd189ded22c52
dianerdiana <dianerdiana.de@gmail.com>
2026-04-04 09:02:59 +0700

    refactor(authentication): remove validation middleware and schemas from authentication routes

--- src/interfaces/http/api/authentication/routes.ts
+++ src/interfaces/http/api/authentication/routes.ts
@@ -1,28 +1,13 @@
 import express from 'express';
 
-import { ValidatorMiddleware } from '../../middleware/validator.middleware.js';
-
 import type { AuthenticationHandler } from './handler.js';
-import { authenticationSchema, loginSchema } from './schema.js';
 
 export const createAuthenticationRouter = (handler: AuthenticationHandler) => {
   const router = express.Router();
 
-  router.post(
-    '/',
-    ValidatorMiddleware.validateBody(loginSchema),
-    handler.postAuthenticationHandler,
-  );
-  router.put(
-    '/',
-    ValidatorMiddleware.validateBody(authenticationSchema),
-    handler.putAuthenticationHandler,
-  );
-  router.delete(
-    '/',
-    ValidatorMiddleware.validateBody(authenticationSchema),
-    handler.deleteAuthenticationHandler,
-  );
+  router.post('/', handler.postAuthenticationHandler);
+  router.put('/', handler.putAuthenticationHandler);
+  router.delete('/', handler.deleteAuthenticationHandler);
 
   return router;
 };
