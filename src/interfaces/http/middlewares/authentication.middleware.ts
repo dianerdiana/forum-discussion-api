e42ca32c4a1320502661ec612b259ca01eda6042
dianerdiana <dianerdiana.de@gmail.com>
2026-04-06 15:29:49 +0700

    feat(authentication): implement AuthenticationMiddleware for user validation

---
+++ src/interfaces/http/middlewares/authentication.middleware.ts
@@ -0,0 +1,37 @@
+import type { NextFunction, Request, Response } from 'express';
+import type { Container } from 'instances-container';
+
+import type { AuthenticationTokenManager } from '@/applications/index.js';
+
+import { AuthenticationError, TOKENS_CONTAINER } from '@/commons/index.js';
+
+export class AuthenticationMiddleware {
+  private readonly container: Container;
+
+  constructor({ container }: { container: Container }) {
+    this.container = container;
+  }
+
+  async validateUser(req: Request, _res: Response, next: NextFunction) {
+    const jwtTokenManager = this.container.getInstance(
+      TOKENS_CONTAINER.authenticationTokenManager,
+    ) as AuthenticationTokenManager;
+
+    const token = req.headers.authorization;
+
+    if (token && token.indexOf('Bearer ') !== -1) {
+      try {
+        const bearerToken = token.split('Bearer ')[1] || '';
+        const user = await jwtTokenManager.decodePayload(bearerToken);
+
+        req.user = user;
+        return next();
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (error) {
+        throw new AuthenticationError('kredensial Anda tidak valid');
+      }
+    }
+
+    throw new AuthenticationError('kredensial Anda tidak valid');
+  }
+}
