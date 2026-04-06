8b7601a9c8881a332661dfdaf696c02842fc6791
dianerdiana <dianerdiana.de@gmail.com>
2026-04-06 17:46:38 +0700

    fix(user-domain-service): rename validateUsername method to verifyUsername for clarity

--- src/domains/services/user-domain.service.ts
+++ src/domains/services/user-domain.service.ts
@@ -5,7 +5,7 @@ import { Username } from '../value-objects/index.js';
 export class UserDomainService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async validateUsername(username: string): Promise<void> {
+  async verifyUsername(username: string): Promise<void> {
     const existingUsername = await this.userRepository.existsByUsername(Username.create(username));
     if (!existingUsername) throw new DomainError('USER.NOT_FOUND');
   }
