27c4cf1d3957bc219540b89354837efd757be824
dianerdiana <dianerdiana.de@gmail.com>
2026-04-07 08:27:23 +0700

    feat(index): export comment and thread repositories from index

--- src/infrastructures/repositories/index.ts
+++ src/infrastructures/repositories/index.ts
@@ -1,2 +1,4 @@
 export { PostgresAuthenticationRepository } from './postgres-authentication.repository.js';
+export { PostgresCommentRepository } from './postgres-comment.repository.js';
+export { PostgresThreadRepository } from './postgres-thread.repository.js';
 export { PostgresUserRepository } from './postgres-user.repository.js';
