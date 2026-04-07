1f93949fd156bd0d4b15e7a6a2335fafaf11f4c9
dianerdiana <dianerdiana.de@gmail.com>
2026-04-07 12:19:51 +0700

    feat(authentication): add verifyAccessToken method to AuthenticationTokenManager interface

--- src/applications/security/authentication-token-manager.ts
+++ src/applications/security/authentication-token-manager.ts
@@ -3,6 +3,7 @@ import type { AuthPayload } from '@/domains/index.js';
 export interface AuthenticationTokenManager {
   createRefreshToken: (payload: AuthPayload) => Promise<string>;
   createAccessToken: (payload: AuthPayload) => Promise<string>;
+  verifyAccessToken: (token: string) => Promise<void>;
   verifyRefreshToken: (token: string) => Promise<void>;
   decodePayload: (payload: string) => Promise<AuthPayload>;
 }
