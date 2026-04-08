efaa1a15f34fd60be558528383ffd516c7ec6efd
dianerdiana <dianerdiana.de@gmail.com>
2026-04-08 22:08:31 +0700

    feat(comment-like-dto): add PerformCommentLikeDto interface and update exports

---
+++ src/applications/dtos/comment-like.dto.ts
@@ -0,0 +1,5 @@
+export interface PerformCommentLikeDto {
+  threadId: string;
+  commentId: string;
+  userId: string;
+}
--- src/applications/dtos/index.ts
+++ src/applications/dtos/index.ts
@@ -1,4 +1,5 @@
 export * from './authentication.dto.js';
 export * from './comment.dto.js';
+export * from './comment-like.dto.js';
 export * from './thread.dto.js';
 export * from './user.dto.js';
